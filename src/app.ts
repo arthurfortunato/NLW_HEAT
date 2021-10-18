1e6fb5bd0f4c98cc09bae35b81d65ad084b05acc
arthurfortunato <luizarthur.fortunato@gmail.com>
2021-10-18 19:51:50 -0300

    criando a rota login github e instalando o dotenv(yarn add dotenv)

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,11 @@
+import 'dotenv/config';
 import express from 'express';;
 
 const app = express();
+
+app.get("/github", (request, response) => {
+  response.redirect(`https://github.com/login/oauth/authorize?client_id=${process.env.GITHUB_CLIENT_ID}`)
+})
 
 app.listen(4000, () => console.log('Server is running on PORT 4000'));
 
