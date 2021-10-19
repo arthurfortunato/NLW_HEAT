30c2758002eb621047cfecdded5c387706add692
arthurfortunato <luizarthur.fortunato@gmail.com>
2021-10-19 19:04:11 -0300

    Retornando as 3 últimas mensagens

--- src/services/GetLast3MessagesService.ts
+++ src/services/GetLast3MessagesService.ts
@@ -2,7 +2,7 @@ import prismaClient from '../prisma';
 
 class GetLast3MessagesService {
   async execute() {
-    const messages = await prismaClient.message.findFirst({
+    const messages = await prismaClient.message.findMany({
       take: 3,
       orderBy: {
         created_at: 'desc'
