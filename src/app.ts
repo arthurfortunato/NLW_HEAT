caaa1990225626d405062c11307de9bf62b66641
arthurfortunato <luizarthur.fortunato@gmail.com>
2021-10-19 18:11:43 -0300

    Instalando o socket io para fazer a comunicação entre o CLient e o Servidor, utilizando o serverHttp

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,16 @@
 import 'dotenv/config';
 import express from 'express';
+import http from 'http';
+import { Server } from 'socket.io';
 
 import { router } from './routes';
 
 const app = express();
+
+const serverHttp = http.createServer(app);
+
+const io = new Server(serverHttp);
+
 app.use(express.json())
 
 app.use(router);
