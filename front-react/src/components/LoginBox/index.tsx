3df3961769b997d78b1aed37b8c0351ef1550825
arthurfortunato <luizarthur.fortunato@gmail.com>
2021-10-20 12:45:51 -0300

    Pegando o código do usuário e limpando a url

--- front-react/src/components/LoginBox/index.tsx
+++ front-react/src/components/LoginBox/index.tsx
@@ -1,3 +1,4 @@
+import { useEffect } from 'react';
 import { VscGithubInverted } from 'react-icons/vsc';
 
 import styles from './styles.module.scss';
@@ -5,6 +6,21 @@ import styles from './styles.module.scss';
 export function LoginBox() {
   const signInUrl = `https://github.com/login/oauth/authorize?scope=user&client_id=108d1eafc8b758b4b573`;
 
+
+
+  useEffect(() => {
+    const url = window.location.href;
+    const hasGithubCode = url.includes('?code=');
+
+    if (hasGithubCode) {
+      const [urlWithoutCode, githubCode] = url.split('?code=')
+
+      console.log({ urlWithoutCode, githubCode })
+
+      window.history.pushState({}, '', urlWithoutCode);
+
+    }
+  })
 
   return (
     <div className={styles.loginBoxWrapper}>
