d7495379f80ff7df2a492a83d133f517f5fce7f3
arthurfortunato <luizarthur.fortunato@gmail.com>
2021-10-20 01:29:06 -0300

    Estilizando o Message List

--- front-react/src/components/MessageList/index.tsx
+++ front-react/src/components/MessageList/index.tsx
@@ -1,5 +1,57 @@
+import styles from './styles.module.scss';
+
+import logoImg from '../../assets/logo.svg';
+
 export function MessageList() {
   return (
-    <h1>MessageList</h1>
+    <div className={styles.messageListWrapper}>
+      <img src={logoImg} alt="Logo" />
+
+      <ul className={styles.messageList}>
+
+        <li className={styles.message}>
+          <p className={styles.messageContent}>
+            Não vejo a hora de começar esse evento, com certeza
+            vai ser o melhor de todos o s tempos,
+            vamooo pra cima!
+          </p>
+          <div className={styles.messageUser}>
+            <div className={styles.userImage}>
+              <img src="https://github.com/arthurfortunato.png" alt="Arthur Fortunato" />
+            </div>
+            <span>Arthur Fortunato</span>
+          </div>
+        </li>
+
+        <li className={styles.message}>
+          <p className={styles.messageContent}>
+            Não vejo a hora de começar esse evento, com certeza
+            vai ser o melhor de todos o s tempos,
+            vamooo pra cima!
+          </p>
+          <div className={styles.messageUser}>
+            <div className={styles.userImage}>
+              <img src="https://github.com/arthurfortunato.png" alt="Arthur Fortunato" />
+            </div>
+            <span>Arthur Fortunato</span>
+          </div>
+        </li>
+
+        <li className={styles.message}>
+          <p className={styles.messageContent}>
+            Não vejo a hora de começar esse evento, com certeza
+            vai ser o melhor de todos o s tempos,
+            vamooo pra cima!
+          </p>
+          <div className={styles.messageUser}>
+            <div className={styles.userImage}>
+              <img src="https://github.com/arthurfortunato.png" alt="Arthur Fortunato" />
+            </div>
+            <span>Arthur Fortunato</span>
+          </div>
+        </li>
+
+      </ul>
+    </div>
   )
 }
