5e474d7579022d25056fd8c2e144746cc6d6806a
arthurfortunato <luizarthur.fortunato@gmail.com>
2021-10-20 11:31:24 -0300

    Atualizando o estado da menssagem

--- front-react/src/components/MessageList/index.tsx
+++ front-react/src/components/MessageList/index.tsx
@@ -3,13 +3,24 @@ import { api } from '../../services/api';
 import styles from './styles.module.scss';
 
 import logoImg from '../../assets/logo.svg';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
+
+type Message = {
+  id: string;
+  text: string;
+  user: {
+    name: string;
+    avatar_url: string;
+  }
+}
 
 export function MessageList() {
+  const [messages, setMessages] = useState<Message[]>([])
+
   useEffect(() => {
     //chamada para api 
-    api.get('messages/last3').then(response => {
-      console.log(response.data);
+    api.get<Message[]>('messages/last3').then(response => {
+      setMessages(response.data)
     })
   }, [])
 
@@ -19,48 +30,20 @@ export function MessageList() {
       <img src={logoImg} alt="Logo" />
 
       <ul className={styles.messageList}>
-
-        <li className={styles.message}>
-          <p className={styles.messageContent}>
-            Não vejo a hora de começar esse evento, com certeza
-            vai ser o melhor de todos o s tempos,
-            vamooo pra cima!
-          </p>
-          <div className={styles.messageUser}>
-            <div className={styles.userImage}>
-              <img src="https://github.com/arthurfortunato.png" alt="Arthur Fortunato" />
-            </div>
-            <span>Arthur Fortunato</span>
-          </div>
-        </li>
-
-        <li className={styles.message}>
-          <p className={styles.messageContent}>
-            Não vejo a hora de começar esse evento, com certeza
-            vai ser o melhor de todos o s tempos,
-            vamooo pra cima!
-          </p>
-          <div className={styles.messageUser}>
-            <div className={styles.userImage}>
-              <img src="https://github.com/arthurfortunato.png" alt="Arthur Fortunato" />
-            </div>
-            <span>Arthur Fortunato</span>
-          </div>
-        </li>
-
-        <li className={styles.message}>
-          <p className={styles.messageContent}>
-            Não vejo a hora de começar esse evento, com certeza
-            vai ser o melhor de todos o s tempos,
-            vamooo pra cima!
-          </p>
-          <div className={styles.messageUser}>
-            <div className={styles.userImage}>
-              <img src="https://github.com/arthurfortunato.png" alt="Arthur Fortunato" />
-            </div>
-            <span>Arthur Fortunato</span>
-          </div>
-        </li>
+        {messages.map(message => {
+          return (
+            <li key={message.id} className={styles.message}>
+              <p className={styles.messageContent}>{message.text}
+              </p>
+              <div className={styles.messageUser}>
+                <div className={styles.userImage}>
+                  <img src={message.user.avatar_url} alt={message.user.name} />
+                </div>
+                <span>{message.user.name}</span>
+              </div>
+            </li>
+          );
+        })}
 
       </ul>
     </div>
