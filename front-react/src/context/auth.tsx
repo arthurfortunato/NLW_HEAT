199eaeab31f16ad368e2c28afad6d7c6f40279e1
arthurfortunato <luizarthur.fortunato@gmail.com>
2021-10-20 13:55:33 -0300

    Passando a responsabilidade de controlar a autenticação da aplicação para o contexto, e o contexto vai provendo as informações necessárias para cada componente da aplicação

--- front-react/src/context/auth.tsx
+++ front-react/src/context/auth.tsx
@@ -1,14 +1,66 @@
-import { createContext, ReactNode } from 'react';
+import { createContext, ReactNode, useEffect, useState } from 'react';
+import { api } from '../services/api';
 
-const AuthContext = createContext(null)
+type User = {
+  id: string;
+  name: string;
+  login: string;
+  avatar_url: string;
+}
+
+type AuthContextData = {
+  user: User | null;
+  signInUrl: string;
+}
+
+export const AuthContext = createContext({} as AuthContextData)
 
 type AuthProvider = {
   children: ReactNode;
 }
 
+type AuthResponse = {
+  token: string;
+  user: {
+    id: string;
+    avatar_url: string;
+    name: string;
+    login: string;
+  }
+}
 export function AuthProvider(props: AuthProvider) {
+  const [user, setUser] = useState<User | null>(null)
+
+  const signInUrl = `https://github.com/login/oauth/authorize?scope=user&client_id=108d1eafc8b758b4b573`;
+
+  async function signIn(githubCode: string) {
+    const response = await api.post<AuthResponse>('authenticate', {
+      code: githubCode,
+    })
+
+    const { token, user } = response.data;
+
+    localStorage.setItem('@dowhile:token', token)
+
+    setUser(user)
+  }
+
+  useEffect(() => {
+    const url = window.location.href;
+    const hasGithubCode = url.includes('?code=');
+
+    if (hasGithubCode) {
+      const [urlWithoutCode, githubCode] = url.split('?code=')
+
+      window.history.pushState({}, '', urlWithoutCode);
+
+      signIn(githubCode)
+
+    }
+  }, [])
+
   return (
-    <AuthContext.Provider value={null}>
+    <AuthContext.Provider value={{ signInUrl, user }}>
       {props.children}
     </AuthContext.Provider>
   );
