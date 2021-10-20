c6353174d039c467a5708e51e675fe2704ed0dba
arthurfortunato <luizarthur.fortunato@gmail.com>
2021-10-20 12:32:41 -0300

    Fazendo Login no github e redirecionando para a página da aplicação

--- front-react/src/components/LoginBox/index.tsx
+++ front-react/src/components/LoginBox/index.tsx
@@ -3,10 +3,13 @@ import { VscGithubInverted } from 'react-icons/vsc';
 import styles from './styles.module.scss';
 
 export function LoginBox() {
+  const signInUrl = `https://github.com/login/oauth/authorize?scope=user&client_id=108d1eafc8b758b4b573`;
+
+
   return (
     <div className={styles.loginBoxWrapper}>
       <strong>Entre e compartilhe a sua mensagem</strong>
-      <a href="#" className={styles.signInWithGithub}>
+      <a href={signInUrl} className={styles.signInWithGithub}>
         <VscGithubInverted size='24px' />
         Entrar com Github
       </a>
