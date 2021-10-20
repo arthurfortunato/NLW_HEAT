975696245e6b8837698f29e62e85b7f88316d801
arthurfortunato <luizarthur.fortunato@gmail.com>
2021-10-20 12:54:26 -0300

    Pegando o token do usuário e enviando para o backend

--- front-react/src/components/LoginBox/index.tsx
+++ front-react/src/components/LoginBox/index.tsx
@@ -1,12 +1,33 @@
 import { useEffect } from 'react';
 import { VscGithubInverted } from 'react-icons/vsc';
+import { api } from '../../services/api';
 
 import styles from './styles.module.scss';
+
+type AuthResponse = {
+  token: string;
+  user: {
+    id: string;
+    avatar_url: string;
+    name: string;
+    login: string;
+  }
+}
 
 export function LoginBox() {
   const signInUrl = `https://github.com/login/oauth/authorize?scope=user&client_id=108d1eafc8b758b4b573`;
 
+  async function signIn(githubCode: string) {
+    const response = await api.post<AuthResponse>('authenticate', {
+      code: githubCode,
+    })
 
+    const { token, user } = response.data;
+
+    localStorage.setItem('@dowhile:token', token)
+
+    console.log(user)
+  }
 
   useEffect(() => {
     const url = window.location.href;
@@ -15,9 +36,9 @@ export function LoginBox() {
     if (hasGithubCode) {
       const [urlWithoutCode, githubCode] = url.split('?code=')
 
-      console.log({ urlWithoutCode, githubCode })
-
       window.history.pushState({}, '', urlWithoutCode);
+
+      signIn(githubCode)
 
     }
   })
