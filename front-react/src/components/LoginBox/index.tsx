a822405fd1ae65284b467092cbd3dc118fa83468
arthurfortunato <luizarthur.fortunato@gmail.com>
2021-10-20 00:44:31 -0300

    Instalando o react-icons

--- front-react/src/components/LoginBox/index.tsx
+++ front-react/src/components/LoginBox/index.tsx
@@ -1,3 +1,5 @@
+import { VscGithubInverted } from 'react-icons/vsc';
+
 import styles from './styles.module.scss';
 
 export function LoginBox() {
@@ -5,6 +7,7 @@ export function LoginBox() {
     <div className={styles.loginBoxWrapper}>
       <strong>Entre e compartilhe a sua mensagem</strong>
       <a href="#" className={styles.signInWithGithub}>
+        <VscGithubInverted size='24px' />
         Entrar com Github
       </a>
 
