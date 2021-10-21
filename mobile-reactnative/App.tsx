6958f10302ac7e282432da3ea64b21f0911507f5
arthurfortunato <luizarthur.fortunato@gmail.com>
2021-10-21 00:17:11 -0300

    Instalando as fontes da aplicação e o AppLoading

--- mobile-reactnative/App.tsx
+++ mobile-reactnative/App.tsx
@@ -1,8 +1,24 @@
 import React from 'react';
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold
+} from '@expo-google-fonts/roboto';
+
+import AppLoading from 'expo-app-loading';
 
 import { Home } from './src/screens/Home';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />
+  }
+
   return (
     <Home />
   );
