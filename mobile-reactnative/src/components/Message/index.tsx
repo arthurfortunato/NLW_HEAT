db5c4239aea7b2752f63555fef0de7b0524c42bd
arthurfortunato <luizarthur.fortunato@gmail.com>
2021-10-21 11:31:46 -0300

    Criando as propriedades da menssagem

--- mobile-reactnative/src/components/Message/index.tsx
+++ mobile-reactnative/src/components/Message/index.tsx
@@ -5,20 +5,33 @@ import { UserPhoto } from '../UserPhoto';
 
 import { styles } from './styles';
 
-export function Message() {
+export type MessageProps = {
+  id: string;
+  text: string;
+  user: {
+    name: string;
+    avatar_url: string;
+  }
+}
+
+type Props = {
+  data: MessageProps;
+}
+
+export function Message({ data }: Props) {
   return (
     <View style={styles.container}>
       <Text style={styles.message}>
-        Texto da Mensagem
+        {data.text}
       </Text>
 
       <View style={styles.footer}>
         <UserPhoto
-          imageUri="https://github.com/arthurfortunato.png"
+          imageUri={data.user.avatar_url}
           sizes="SMALL" />
 
         <Text style={styles.userName}>
-          Nome do usuário
+          {data.user.name}
         </Text>
 
       </View>
--- mobile-reactnative/src/components/MessageList/index.tsx
+++ mobile-reactnative/src/components/MessageList/index.tsx
@@ -7,15 +7,25 @@ import { Message } from '../Message';
 import { styles } from './styles';
 
 export function MessageList() {
+
+  const message = {
+    id: '1',
+    text: 'Mensagem de teste',
+    user: {
+      name: 'Arthur Fortunato',
+      avatar_url: 'https://github.com/arthurfortunato.png',
+    }
+  }
   return (
     <ScrollView
       style={styles.container}
       contentContainerStyle={styles.content}
       keyboardShouldPersistTaps="never"
     >
-      <Message />
-      <Message />
-      <Message />
+      <Message data={message} />
+      <Message data={message} />
+      <Message data={message} />
+
     </ScrollView>
   );
 }
