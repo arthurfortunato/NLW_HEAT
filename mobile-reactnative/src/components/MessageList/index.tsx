895059b9a34a5318d22da09e98317551463cfad5
arthurfortunato <luizarthur.fortunato@gmail.com>
2021-10-22 15:13:27 -0300

    Renderizando as mensagens em tempo real

--- mobile-reactnative/src/components/MessageList/index.tsx
+++ mobile-reactnative/src/components/MessageList/index.tsx
@@ -1,11 +1,18 @@
 import React, { useState, useEffect } from 'react';
-
 import { ScrollView } from 'react-native';
 import { api } from '../../services/api';
+import { io } from 'socket.io-client';
 
 import { Message, MessageProps } from '../Message';
 
 import { styles } from './styles';
+let messagesQueue: MessageProps[] = [];
+
+const socket = io(String(api.defaults.baseURL));
+socket.on('new_message', (newMessage) => {
+  messagesQueue.push(newMessage);
+  console.log(newMessage)
+})
 
 export function MessageList() {
 
@@ -18,6 +25,17 @@ export function MessageList() {
     }
     fetchMessages();
   }, []);
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      if (messagesQueue.length > 0) {
+        setCurrentMessages(prevState => [messagesQueue[0], prevState[0], prevState[1]]);
+        messagesQueue.shift();
+      }
+    }, 3000)
+
+    return () => clearInterval(timer);
+  }, [])
 
   return (
     <ScrollView
