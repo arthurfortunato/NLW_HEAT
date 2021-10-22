ad89ed1ed434cc7635f6b4720b5b8b79a5390699
arthurfortunato <luizarthur.fortunato@gmail.com>
2021-10-21 21:13:56 -0300

    Criando o componente SignBox e inegrando o Button

--- mobile-reactnative/src/screens/Home/index.tsx
+++ mobile-reactnative/src/screens/Home/index.tsx
@@ -1,16 +1,18 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
 
 import { styles } from './styles';
 
 import { Header } from '../../components/Header';
 import { MessageList } from '../../components/MessageList';
+import { SignInBox } from '../../components/SigninBox';
 
 export function Home() {
   return (
     <View style={styles.container}>
       <Header />
       <MessageList />
+      <SignInBox />
     </View>
   )
 }
