6ee6dad326dad00ba6653afc258ef150e1f3d8bc
arthurfortunato <luizarthur.fortunato@gmail.com>
2021-10-22 14:34:40 -0300

    Pegando as mensagens do backend

--- mobile-reactnative/src/components/MessageList/index.tsx
+++ mobile-reactnative/src/components/MessageList/index.tsx
@@ -1,30 +1,31 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 import { ScrollView } from 'react-native';
+import { api } from '../../services/api';
 
-import { Message } from '../Message';
+import { Message, MessageProps } from '../Message';
 
 import { styles } from './styles';
 
 export function MessageList() {
 
-  const message = {
-    id: '1',
-    text: 'Mensagem de teste',
-    user: {
-      name: 'Arthur Fortunato',
-      avatar_url: 'https://github.com/arthurfortunato.png',
+  const [currentMessages, setCurrentMessages] = useState<MessageProps[]>([]);
+
+  useEffect(() => {
+    async function fetchMessages() {
+      const messagesResponse = await api.get<MessageProps[]>('/messages/last3');
+      setCurrentMessages(messagesResponse.data)
     }
-  }
+    fetchMessages();
+  }, []);
+
   return (
     <ScrollView
       style={styles.container}
       contentContainerStyle={styles.content}
       keyboardShouldPersistTaps="never"
     >
-      <Message data={message} />
-      <Message data={message} />
-      <Message data={message} />
+      {currentMessages.map((message) => <Message key={message.id} data={message} />)}
 
     </ScrollView>
   );
